557c33eecb272926ed56121b06ea6754515186db
evelyne <evelyne.latremoliere@gmail.com>
2019-03-12 15:34:41 +0100

    khk

--- pagination.js
+++ pagination.js
@@ -1,8 +1,5 @@
 async function init() {
-    alert("zut");
     const navigation=await fetch("theme.html");
-    alert("rezut");
     const aecrire=await navigation.text();
-    alert("rerezut");
     document.body.insertAdjacentHTML('afterbegin',aecrire);
 }
