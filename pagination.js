e47f1959815b1b465f078ca89ff20e336cdaefd1
evelyne <evelyne.latremoliere@gmail.com>
2019-03-13 23:03:26 +0100

    ghkj

--- pagination.js
+++ pagination.js
@@ -1,5 +1,10 @@
 async function init() {
-    const navigation=await fetch("theme.html");
+    var myHeaders = new Headers();
+    var myInit = { method: 'GET',
+               headers: myHeaders,
+               mode: 'navigate',
+               cache: 'default' };
+    const navigation=await fetch("theme.html",myInit);
     const aecrire=await navigation.text();
     document.body.insertAdjacentHTML('afterbegin',aecrire);
 }
